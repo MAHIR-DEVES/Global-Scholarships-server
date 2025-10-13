7f53e74f20c480d45217dfbde3323ff01ca26622
ana-yet <anayethusenaj@gmail.com>
2025-10-14 01:38:59 +0600

    created delete methog by id

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -4,6 +4,7 @@ import {
   getBlogPosts,
   getBlogPostBySlug,
   updateBlogPost,
+  deleteBlogPost,
 } from "../controllers/blogPost.js";
 
 const router = Router();
@@ -11,6 +12,7 @@ const router = Router();
 router.post("/", createBlogPost); // POST /api/blog
 router.get("/", getBlogPosts); // GET /api/blog
 router.get("/:slug", getBlogPostBySlug); // GET /api/blog/:slug
-router.patch("/:id", updateBlogPost); // GET /api/blog/:slug
+router.patch("/:id", updateBlogPost); // patch /api/blog/:id
+router.delete("/:id", deleteBlogPost); // delete /api/blog/:id
 
 export default router;
